b5a6cdec3119aead433bf97629bb5d20b8105f28
toannq13 <toanguyen200220@gmail.com>
2025-02-27 22:04:52 +0700

    Initial Commit

---
+++ MyToDoApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyToDoApp
+//
+//  Created by Toán Nguyễn on 27/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyToDoApp/MyToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyToDoAppApp.swift
+//  MyToDoApp
+//
+//  Created by Toán Nguyễn on 27/2/25.
+//
+
+import SwiftUI
+
+@main
+struct MyToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
